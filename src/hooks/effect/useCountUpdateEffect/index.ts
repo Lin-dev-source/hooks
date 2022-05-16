a7aa099c184ba01e09d141400f603258ac71bbb7
YAJUENET\gzs21289 <1244715423@qq.com>
2022-05-16 11:06:13 +0800

    格式化相关代码

--- src/hooks/effect/useCountUpdateEffect/index.ts
+++ src/hooks/effect/useCountUpdateEffect/index.ts
@@ -1,15 +1,15 @@
-import { useEffect, useRef } from 'react';
-import type { DependencyList, EffectCallback } from 'react';
+import { useEffect, useRef } from "react";
+import type { DependencyList, EffectCallback } from "react";
 
 // 第n次渲染并且deps变化才执行
 export default function useCountUpdateEffect(effect: EffectCallback, n: number, deps?: DependencyList): void {
-  const count = useRef<number>(0)
+	const count = useRef<number>(0);
 
-  count.current = count.current + 1;
-  useEffect(() => {
-    // console.log('useEffect',count.current);
-    if (count.current == n) {
-      return effect()
-    }
-  }, deps)
+	count.current = count.current + 1;
+	useEffect(() => {
+		// console.log('useEffect',count.current);
+		if (count.current === n) {
+			return effect();
+		}
+	}, deps);
 }
--- src/pluto-hooks.ts
+++ src/pluto-hooks.ts
@@ -1,72 +1,72 @@
 // effect
-import useAsyncEffect from "./hooks/effect/useAsyncEffect"
-import useCompareEffect from "./hooks/effect/useCompareEffect"
-import useDeepCompareEffect from "./hooks/effect/useDeepCompareEffect"
-import useDebounceEffect from "./hooks/effect/useDebounceEffect"
-import useDebounceFn from "./hooks/effect/useDebounceFn"
-import useEffectOnce from "./hooks/effect/useEffectOnce"
-import useThrottleFn from "./hooks/effect/useThrottleFn"
-import useThrottleEffect from "./hooks/effect/useThrottleEffect"
-import useUpdateEffect from "./hooks/effect/useUpdateEffect"
-import useCountUpdateEffect from "./hooks/effect/useCountUpdateEffect"
-import useRangeUpdateEffect from "./hooks/effect/useCountUpdateEffect"
+import useAsyncEffect from "./hooks/effect/useAsyncEffect";
+import useCompareEffect from "./hooks/effect/useCompareEffect";
+import useDeepCompareEffect from "./hooks/effect/useDeepCompareEffect";
+import useDebounceEffect from "./hooks/effect/useDebounceEffect";
+import useDebounceFn from "./hooks/effect/useDebounceFn";
+import useEffectOnce from "./hooks/effect/useEffectOnce";
+import useThrottleFn from "./hooks/effect/useThrottleFn";
+import useThrottleEffect from "./hooks/effect/useThrottleEffect";
+import useUpdateEffect from "./hooks/effect/useUpdateEffect";
+import useCountUpdateEffect from "./hooks/effect/useCountUpdateEffect";
+import useRangeUpdateEffect from "./hooks/effect/useRangeUpdateEffect";
 
-//state
-import useBoolean from "./hooks/state/useBoolean"
-import useCookie from "./hooks/state/useCookie"
-import useCookies from "./hooks/state/useCookies"
-import useGetState from "./hooks/state/useGetState"
-import useLocalStorageState from "./hooks/state/useLocalStorageState"
-import useMap from "./hooks/state/useMap"
-import usePrevious from "./hooks/state/usePrevious"
-import useSessionStorageState from "./hooks/state/useSessionStorageState"
-import useSet from "./hooks/state/useSet"
-import useSetState from "./hooks/state/useSetState"
-import useToggle from "./hooks/state/useToggle"
-import useUrlState from "./hooks/state/useUrlState"
+// state
+import useBoolean from "./hooks/state/useBoolean";
+import useCookie from "./hooks/state/useCookie";
+import useCookies from "./hooks/state/useCookies";
+import useGetState from "./hooks/state/useGetState";
+import useLocalStorageState from "./hooks/state/useLocalStorageState";
+import useMap from "./hooks/state/useMap";
+import usePrevious from "./hooks/state/usePrevious";
+import useSessionStorageState from "./hooks/state/useSessionStorageState";
+import useSet from "./hooks/state/useSet";
+import useSetState from "./hooks/state/useSetState";
+import useToggle from "./hooks/state/useToggle";
+import useUrlState from "./hooks/state/useUrlState";
 
 // leftCycle
-import useFirstMountState from "./hooks/lifeCycle/useFirstMountState"
-import useMount from "./hooks/lifeCycle/useMount"
-import useUnmount from "./hooks/lifeCycle/useUnmount"
-import useUnmountedRef from "./hooks/lifeCycle/useUnmountedRef"
+import useFirstMountState from "./hooks/lifeCycle/useFirstMountState";
+import useMount from "./hooks/lifeCycle/useMount";
+import useUnmount from "./hooks/lifeCycle/useUnmount";
+import useUnmountedRef from "./hooks/lifeCycle/useUnmountedRef";
 
 // other
-import useEvent from "./hooks/callBack/useEvent"
+import useEvent from "./hooks/callBack/useEvent";
 
 export {
-  useAsyncEffect,
-  useCompareEffect,
-  useDeepCompareEffect,
-  useDebounceEffect,
-  useDebounceFn,
-  useEffectOnce,
-  useThrottleFn,
-  useThrottleEffect,
-  useUpdateEffect,
-  useCountUpdateEffect,
-  useRangeUpdateEffect,
+	useAsyncEffect,
+	useCompareEffect,
+	useDeepCompareEffect,
+	useDebounceEffect,
+	useDebounceFn,
+	useEffectOnce,
+	useThrottleFn,
+	useThrottleEffect,
+	useUpdateEffect,
+	useCountUpdateEffect,
+	useRangeUpdateEffect,
 
-  // state
-  useBoolean,
-  useCookie,
-  useCookies,
-  useGetState,
-  useLocalStorageState,
-  useMap,
-  usePrevious,
-  useSessionStorageState,
-  useSet,
-  useSetState,
-  useToggle,
-  useUrlState,
+	// state
+	useBoolean,
+	useCookie,
+	useCookies,
+	useGetState,
+	useLocalStorageState,
+	useMap,
+	usePrevious,
+	useSessionStorageState,
+	useSet,
+	useSetState,
+	useToggle,
+	useUrlState,
 
-  // lefeCycle
-  useFirstMountState,
-  useMount,
-  useUnmount,
-  useUnmountedRef,
+	// lefeCycle
+	useFirstMountState,
+	useMount,
+	useUnmount,
+	useUnmountedRef,
 
-  // other
-  useEvent
-}
+	// other
+	useEvent
+};
